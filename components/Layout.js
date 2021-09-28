5bf70ef42ed35eca9f68328c3ceed5094466d979
abdurrahman200 <abdur.rahman200301@gmail.com>
2021-09-28 22:31:43 +0600

    some change

--- components/Layout.js
+++ components/Layout.js
@@ -6,24 +6,24 @@ import Footer from './Footer'
 import Header from './Header'
 
 export default function Layout({ title, keywords, description, children }) {
-   const router = useRouter()
-   return (
-      <div>
-         <Head>
-            <title>{title}</title>
-            <meta name='description' content={description} />
-            <meta name='keywords' content={keywords} />
-         </Head>
-         <Header />
-         {router.pathname === '/' && <Showcase />}
-         <div className={styles.container}>{children}</div>
-         <Footer />
-      </div>
-   )
+  const router = useRouter()
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+      </Head>
+      <Header />
+      {router.pathname === '/' && <Showcase />}
+      <div className={styles.container}>{children}</div>
+      <Footer />
+    </div>
+  )
 }
 
 Layout.defaultProps = {
-   title: 'Dhaka Nightlife | Find the hottest parties',
-   description: 'Find the lates and other musical Dhaka Nightlife',
-   keywords: 'music, edm, Dhaka Nightlife',
+  title: 'Disco | Find the hottest parties',
+  description: 'Find the lates and other musical Dhaka Nightlife',
+  keywords: 'music, edm, Disco',
 }
