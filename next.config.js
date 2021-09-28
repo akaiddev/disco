44f6f4871fd7b61c555323209f33c1f0d5e7fd51
abdurrahman200 <abdur.rahman200301@gmail.com>
2021-09-28 22:47:23 +0600

    some change

--- next.config.js
+++ next.config.js
@@ -8,4 +8,13 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+    }
+  },
 }
