3cbabc29c0f96b810ad92eceb2cfab607e9d4eda
abdurrahman200 <abdur.rahman200301@gmail.com>
2021-09-28 22:56:11 +0600

    getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ events }) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`)
   const events = await res.json()
 
